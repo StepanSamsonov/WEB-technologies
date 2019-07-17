4300a710e9ab6cba9e90ef9038816121b6401a26
StepanSamsonov <mynameisericlensherr@gmail.com>
2019-07-17 20:02:44 +0000

    Some content

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
+git config --global user.name StepanSamsonov
+git config --global user.email mynameisericlensherr@gmail.com
 ﻿sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo nginx -t
 sudo /etc/init.d/nginx restart
