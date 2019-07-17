34f8e7fd627336aa04663457b9ed77697cd25d2a
Stepan Samsonov <stepan.samsonov@orange.com>
2019-07-17 22:44:20 +0300

    Some content

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
-﻿sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/nginx.conf
+﻿sudo rm -rf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/nginx.conf
 sudo nginx -t
 sudo /etc/init.d/nginx restart
