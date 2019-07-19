7b0a415d7e86a4fc11917c8f1b133a7167f05cda
Stepan Samsonov <stepan.samsonov@orange.com>
2019-07-20 00:06:40 +0300

    Lesson 1.9

--- hello.py
+++ hello.py
@@ -1,13 +1,9 @@
-def parse_query(query):
-    return '\n'.join(query.split('&'))
-
-
 def wsgi_hello(environ, start_response):
     status = '200 OK'
     headers = [
         ('Content-Type', 'text/plain')
     ]
-    body = parse_query(environ['QUERY_STRING'])
+    body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
 
     start_response(status, headers)
     return [body]
