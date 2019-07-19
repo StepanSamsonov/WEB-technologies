f45085572b6a6b08a1fffb0d1f0ce5c37da02eee
StepanSamsonov <mynameisericlensherr@gmail.com>
2019-07-19 21:35:07 +0000

    Lesson 1.9

--- init.sh
+++ init.sh
@@ -1,4 +1,9 @@
 git config --global user.name StepanSamsonov
 git config --global user.email mynameisericlensherr@gmail.com
+pip install gunicorn
 ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 /etc/init.d/nginx restart
+
+sudo ln -s /home/box/web/etc/gunicorn.py /etc/gunicorn.d/gunicorn.py
+
+gunicorn hello:wsgi_hello
