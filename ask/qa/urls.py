75f5956439d2483915bbfd3da1d7544f3683ce18
StepanSamsonov <mynameisericlensherr@gmail.com>
2019-09-15 19:56:36 +0000

    Lesson 2.1

---
+++ ask/qa/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('/', views.test),
+    path('/login/', views.test),
+    path('/signup/', views.test),
+    path('/question/<ID>/', views.test),
+    path('/ask/', views.test),
+    path('/popular/', views.test),
+    path('/new/', views.test),
+]
