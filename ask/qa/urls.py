34b1c91e72ba5133e4b127e851e63a9d7b7601ef
StepanSamsonov <mynameisericlensherr@gmail.com>
2019-09-15 21:06:52 +0000

    Lesson 2.1

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('/', views.test),
     path('/login/', views.test),
     path('/signup/', views.test),
-    path('/question/<ID>/', views.test),
+    path('/question/<int:id>/', views.test),
     path('/ask/', views.test),
     path('/popular/', views.test),
     path('/new/', views.test),
