593310c17116d29f6a9a9a7085da7884fc547986
StepanSamsonov <mynameisericlensherr@gmail.com>
2019-10-05 11:15:41 +0000

    Lesson 2.3

--- setupMySQL.sh
+++ setupMySQL.sh
@@ -1,3 +1,5 @@
 sudo /etc/init.d/mysql start
-mysql -uroot -e "create database ask"
+mysql -uroot -e "create database ask;"
+mysql -uroot -e "grant all privileges on ask.* to 'box'@'localhost' with grant option;"
+
 
