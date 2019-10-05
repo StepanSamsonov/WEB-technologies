9545edba59c28c113b84c2e36fe9f403cd293c8f
StepanSamsonov <mynameisericlensherr@gmail.com>
2019-10-05 18:58:24 +0000

    Lesson 2.5

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -8,20 +8,33 @@ def test(request, *args, **kwargs):
     return HttpResponse('OK')
 
 def page(request):
-	paginator = Paginator(Question.new, 10)
+	paginator = Paginator(Question.objects.new(), 10)
 	page = request.GET.get('page')
+	
+	try:
+		page = int(page)
+	except:
+		page = 1
+
 	questions = paginator.page(page)
 
-	return render(request, 'question/page.html', {
+
+	return render(request, 'page.html', {
 		'questions': questions,
 	})
 
 def popular_page(request):
-	paginator = Paginator(Question.popular, 10)
+	paginator = Paginator(Question.objects.popular(), 10)
 	page = request.GET.get('page')
+
+	try:
+		page = int(page)
+	except:
+		page = 1
+
 	questions = paginator.page(page)
 
-	return render(request, 'question/popular_page.html', {
+	return render(request, 'popular_page.html', {
 		'questions': questions,
 	})
 
@@ -31,6 +44,6 @@ def question_page(request, id):
 	except Question.DoesNotExist:
 		raise Http404('Not found')
 	
-	return render(request, 'question/question_page.html', {
+	return render(request, 'question_page.html', {
 		'question': question,
 	})
